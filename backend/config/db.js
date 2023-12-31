2ac736baaf78523859a56a72d2a40085713d6f8f
generalentropy <entropie@tuta.io>
2023-12-31 15:20:01 +0100

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
